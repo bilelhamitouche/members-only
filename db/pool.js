ff954918bdc2603f5d0321c67cd886e2f7f9e6fc
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-17 19:39:33 +0100

    fixed a typo in the env file that broke the signup and db

--- db/pool.js
+++ db/pool.js
@@ -5,6 +5,6 @@ module.exports = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
