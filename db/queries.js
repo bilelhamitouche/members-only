e9e990a03f1bb9459260614d06c2825955b203ba
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-17 14:06:28 +0100

    added an insert user method

--- db/queries.js
+++ db/queries.js
@@ -2,8 +2,17 @@ const pool = require("./pool");
 
 async function getPosts() {
   const { rows } = await pool.query("SELECT * FROM posts");
+  return rows;
+}
+
+async function insertUser(firstName, lastName, username, password) {
+  await pool.query(
+    "INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)",
+    [firstName, lastName, username, password],
+  );
 }
 
 module.exports = {
   getPosts,
+  insertUser,
 };
