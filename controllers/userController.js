7b8d273243610e2f5ff3a326201e618b6a98dfbb
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-17 19:38:37 +0100

    added post endpoint for signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,33 @@
+const bcrypt = require("bcrypt");
 const db = require("../db/queries");
 
-async function getLoginPage(req, res) {
+async function loginUserGet(req, res) {
   res.render("login", { title: "Login" });
 }
 
-async function getSignUpPage(req, res) {
+async function signUpUserGet(req, res) {
   res.render("signup", { title: "Sign up" });
 }
 
+async function signUpUserPost(req, res, next) {
+  const { firstname, lastname, username, password } = req.body;
+  console.log(req.body);
+  bcrypt.hash(password, 10, async (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    } else {
+      try {
+        await db.insertUser(firstname, lastname, username, hashedPassword);
+        res.redirect("/users/login");
+      } catch (err) {
+        return next(err);
+      }
+    }
+  });
+}
+
 module.exports = {
-  getLoginPage,
-  getSignUpPage,
+  loginUserGet,
+  signUpUserGet,
+  signUpUserPost,
 };
