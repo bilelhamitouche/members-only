2b6ac5832d687e8071218438bef1415175b2ef0a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-18 00:43:59 +0100

    added passport and login form functionality

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require("passport");
 const {
   loginUserGet,
   signUpUserGet,
@@ -10,5 +11,13 @@ const userRouter = express.Router();
 userRouter.get("/login", loginUserGet);
 userRouter.get("/signup", signUpUserGet);
 userRouter.post("/signup", signUpUserPost);
+userRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/posts",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  }),
+);
 
 module.exports = userRouter;
