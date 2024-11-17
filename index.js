5ee804ec1f984965874f5cce27e5d981e44214a1
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-17 19:40:37 +0100

    fixes the pgStore

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     store: new pgSession({
       pool: pool,
       tableName: "session",
