f9896e4a3ad8e25ed85bd25767094d82a6351fc9
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-17 14:05:57 +0100

    added some middleware to the main file

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const pool = require("./db/pool");
+const indexRouter = require("./routes/indexRouter");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,9 +14,20 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-app.use(express.session());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new pgSession({
+      pool: pool,
+      tableName: "session",
+    }),
+  }),
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use("/", indexRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
