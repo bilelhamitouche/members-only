d6d17872ab48c3beb3f4b02f30334707cd314701
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-18 02:55:29 +0100

    added a controller function that handles creating posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,6 +5,19 @@ async function getPosts(req, res) {
   res.render("posts", { title: "Posts", posts: posts });
 }
 
+async function createPostGet(req, res) {
+  res.render("createPost", { title: "Create post", user: req.user });
+}
+
+async function createPostPost(req, res) {
+  const user = req.user;
+  const { title, text } = req.body;
+  await db.insertPost(title, text, user.id);
+  res.redirect("/posts");
+}
+
 module.exports = {
   getPosts,
+  createPostGet,
+  createPostPost,
 };
