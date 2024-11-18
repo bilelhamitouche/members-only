78318a82e31670ee87c11b2e38baa724fda89271
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-19 00:14:27 +0100

    added validation for firstname lastname and username

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,59 @@
 const bcrypt = require("bcrypt");
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const emailErr = "must be a valid Email.";
+const lengthErr = "must be between 2 and 16 characters";
+
+const validateUser = [
+  body("firstname")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 2, max: 16 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastname")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 2, max: 16 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("username").trim().isEmail().withMessage(`username ${emailErr}`),
+];
 
 async function loginUserGet(req, res) {
   res.render("login", { title: "Login", message: req.flash("error") });
 }
 
 async function signUpUserGet(req, res) {
-  res.render("signup", { title: "Sign up" });
+  res.render("signup", { title: "Sign up", errors: [] });
 }
 
-async function signUpUserPost(req, res, next) {
-  const { firstname, lastname, username, password } = req.body;
-  bcrypt.hash(password, 10, async (err, hashedPassword) => {
-    if (err) {
-      return next(err);
-    } else {
-      try {
-        await db.insertUser(firstname, lastname, username, hashedPassword);
-        res.redirect("/users/login");
-      } catch (err) {
-        return next(err);
-      }
+const signUpUserPost = [
+  validateUser,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .render("signup", { title: "Sign up", errors: errors.array() });
     }
-  });
-}
+    const { firstname, lastname, username, password } = req.body;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      } else {
+        try {
+          await db.insertUser(firstname, lastname, username, hashedPassword);
+          res.redirect("/users/login");
+        } catch (err) {
+          return next(err);
+        }
+      }
+    });
+  },
+];
 
 async function logoutUserGet(req, res, next) {
   req.logout((err) => {
@@ -43,12 +73,17 @@ function becomeMemberGet(req, res) {
 }
 
 async function becomeMemberPost(req, res) {
+  const passwords = [
+    "lionel messi",
+    "cristiano ronaldo",
+    "ronaldo",
+    "messi",
+    "cr7",
+    "lm10",
+  ];
   const id = req.user.id;
   const { password } = req.body;
-  if (
-    password.toLowerCase() === "lionel messi" ||
-    password.toLowerCase() === "cristiano ronaldo"
-  ) {
+  if (passwords.includes(password.toLowerCase())) {
     await db.becomeMember(id);
   } else {
     res.redirect("/users/become-member");
