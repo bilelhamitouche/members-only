024a401c8e5b537c61833affa23b0bbabe07e018
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-18 02:55:18 +0100

    added a few routes for creating posts

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { getPosts } = require("../controllers/postController");
+const {
+  getPosts,
+  createPostGet,
+  createPostPost,
+} = require("../controllers/postController");
 
 const postRouter = express.Router();
 
 postRouter.get("/", getPosts);
+postRouter.get("/create", createPostGet);
+postRouter.post("/create", createPostPost);
 
 module.exports = postRouter;
