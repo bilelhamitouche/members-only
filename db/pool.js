acd8aa0e63e3a9d1ee97265b82b48b5cded03149
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2024-11-19 14:56:47 +0100

    replaced the pool options with connection string

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
