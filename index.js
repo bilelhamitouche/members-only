187b9f2e734ca39109b81f69d5191eea92bc0a00
Bilel Hamitouche <bilelhamitouche>
2024-12-07 01:49:38 +0100

    changed the order of things and deleted the authentication folder

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const db = require("./db/queries");
 const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const pgSession = require("connect-pg-simple")(session);
 const flash = require("connect-flash");
@@ -29,6 +32,39 @@ app.use(
     }),
   }),
 );
+
+async function verifyCallback(username, password, done) {
+  try {
+    const user = await db.getUserByUsername(username);
+    if (!user) {
+      return done(null, false, { message: "Incorrect username, try again!" });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return done(null, false, { message: "Incorrect password, try again!" });
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}
+
+const passportLocalStrategy = new LocalStrategy(verifyCallback);
+passport.use(passportLocalStrategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (userId, done) => {
+  try {
+    const user = await db.getUserById(userId);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 app.use(flash());
 app.use(passport.session());
 
